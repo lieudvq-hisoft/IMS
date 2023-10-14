1c3770234cbd81d2a60539e8cc6820484fbb8633
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-10-14 22:45:11 +0700

    fix

--- Data/Models/LocationModel.cs
+++ Data/Models/LocationModel.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace Data.Models;
 public class AreaModel
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
     public int RowCount { get; set; }
     public int ColumnCount { get; set; }
 }
