e36b30a6f523129884f1304b06693172fcfe06c5
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-12-04 23:15:11 +0700

    Update CustomerModel.cs

--- Data/Models/CustomerModel.cs
+++ Data/Models/CustomerModel.cs
@@ -19,12 +19,12 @@ public class CustomerModel
 public class CustomerResultModel
 {
     public int Id { get; set; }
-    //public string? CompanyName { get; set; }
+    public string? CompanyName { get; set; }
     public string? Address { get; set; }
     public string? TaxNumber { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
-    public string? CustomerName { get; set; }
+    //public string? CustomerName { get; set; }
 
     //public int? CompanyTypeId { get; set; }
 }
