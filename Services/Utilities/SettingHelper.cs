8f0bb8ea3889be9b0675a3b2515700687cba355a
JSum-PhamNhatHa <jsum.webdev@gmail.com>
2023-12-07 18:15:22 +0700

    đền tội

--- Services/Utilities/SettingHelper.cs
+++ Services/Utilities/SettingHelper.cs
@@ -1,4 +1,13 @@
-﻿namespace Services.Utilities;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Utilities;
 public class SettingHelper
 {
     public static string GetEnvironment()
