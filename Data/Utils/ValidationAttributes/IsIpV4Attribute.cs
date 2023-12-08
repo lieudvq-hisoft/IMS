340022d898ec3b73940761b64d72043a707fd9ea
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-12-08 15:35:54 +0700

    update

--- Data/Utils/ValidationAttributes/IsIpV4Attribute.cs
+++ Data/Utils/ValidationAttributes/IsIpV4Attribute.cs
@@ -7,7 +7,8 @@ internal class IsIpV4Attribute : ValidationAttribute
 {
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        var pattern = new Regex(@"^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$");
+        //var pattern = new Regex(@"^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$");
+        var pattern = new Regex(@"^(19[2-9]|2[0-1][0-9]|22[0-3])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.0$");
         if (value is string ipString && pattern.IsMatch(ipString))
         {
             return ValidationResult.Success;
