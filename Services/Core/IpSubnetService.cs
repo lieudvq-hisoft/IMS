cedec55d6e8081ecc898153787a36011d7ed00bc
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-12-18 21:14:33 +0700

    Update IpSubnetService.cs

--- Services/Core/IpSubnetService.cs
+++ Services/Core/IpSubnetService.cs
@@ -184,6 +184,7 @@ public class IpSubnetService : IIpSubnetService
             var subnetIps = _dbContext.IpAddresses
                 .Include(x => x.IpAssignments).ThenInclude(x => x.ServerAllocation).ThenInclude(x => x.Customer)
                 .Include(x => x.RequestHostIps).ThenInclude(x => x.RequestHost)
+                .OrderBy(x => x.Address)
                 .Where(x => x.IpSubnetId == subnet.Data.Id)
                 .Where(x => purposes != null && purposes.Count > 0 ? purposes.Contains(x.Purpose) : true)
                 .ToList();
@@ -193,6 +194,7 @@ public class IpSubnetService : IIpSubnetService
                 var childSubnetIps = _dbContext.IpAddresses
                     .Include(x => x.IpAssignments).ThenInclude(x => x.ServerAllocation).ThenInclude(x => x.Customer)
                     .Include(x => x.RequestHostIps).ThenInclude(x => x.RequestHost)
+                    .OrderBy(x => x.Address)
                     .Where(x => x.IpSubnetId == ipSubnet.Id)
                     .Where(x => purposes != null && purposes.Count > 0 ? purposes.Contains(x.Purpose) : true)
                     .ToList();
@@ -641,6 +643,7 @@ public class IpSubnetService : IIpSubnetService
                         var ipAddresses = _dbContext.IpAddresses
                             .Include(x => x.IpAssignments)
                             .Include(x => x.RequestHostIps).ThenInclude(x => x.RequestHost)
+                            .OrderBy(x => x.Address)
                             .Where(x => !x.Blocked && !x.IsReserved && !x.IpAssignments.Any() && !x.RequestHostIps.Select(x => x.RequestHost).Any(x => x.Status == RequestHostStatus.Waiting || x.Status == RequestHostStatus.Accepted || x.Status == RequestHostStatus.Processed))
                             .Where(x => model.Purposes != null && model.Purposes.Count > 0 ? model.Purposes.Contains(x.Purpose) : true)
                             .Where(x => !additionalIpIds.Contains(x.Id));
